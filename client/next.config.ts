71a0ccdf6500d412b9f913a045fe1d25a2defbac
hamzaMissewi <hamza.missaoui47@gmail.com>
2025-07-17 06:44:45 +0100

    refactor layout.tsx for improved formatting and added creator metadata

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,22 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "example.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "*.amazonaws.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'example.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: '*.amazonaws.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
